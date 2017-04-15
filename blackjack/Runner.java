417d9891c1a2194d74e303ab72a723201a315a2d
Dominic <mrdjfraser@gmail.com>
2017-04-15 16:00:26 +0100

    simpleWin Runner logic set up

--- blackjack/Runner.java
+++ blackjack/Runner.java
@@ -7,6 +7,9 @@ public class Runner{
   int amountOfPlayers = game.getHowManyPlayers();
   game.createPlayers(amountOfPlayers);
 
+  game.deal();
+  Player winner = WinChecker.simpleWin(game);
+  System.out.println("The winner is: " + winner.getPlayerName() + "!");
 
 
   }
